a0c6f394b05a635dc7717a22521420adbf8e0348
Priyanshu Indra <priyanshuindra3@gmail.com>
2023-05-08 18:41:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+// Importing Components
+import Header from './components/Layout/Header'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+    </>
+  )
+}
+
+export default App
