a2e6f070bc0d6602c13a9c05dd5abc4c6c669951
Priyanshu Indra <priyanshuindra3@gmail.com>
2023-05-10 17:58:29 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FoodOrdering_React_App/",
+  base: "/https://priyanshuindra.github.io/FoodOrdering_React_App//",
   plugins: [react()],
 })
